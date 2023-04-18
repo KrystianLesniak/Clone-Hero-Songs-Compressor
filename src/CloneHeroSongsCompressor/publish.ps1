cdc5353a9030687a72d821f24c296ddded24b1c0
Krystian Leśniak <kontakt@krystianlesniak.pl>
2023-04-18 13:05:57 +0200

    Improved publish script

--- src/CloneHeroSongsCompressor/publish.ps1
+++ src/CloneHeroSongsCompressor/publish.ps1
@@ -1,1 +1,26 @@
-dotnet publish CloneHeroSongsCompressor.csproj -c Release -f net7.0-windows10.0.22621.0 -p:WindowsPackageType=None -p:SelfContained=true -p:WindowsAppSDKSelfContained=true
+try
+{
+$NetWindowsVer = "net7.0-windows10.0.22621.0"
+$NewBinLocation = "..\..\..\CHSongCompressorPublish\win10-x64"
+
+dotnet publish CloneHeroSongsCompressor.csproj -c Release --os win10 -a x64 --self-contained -f $NetWindowsVer -p:WindowsPackageType=None -p:WindowsAppSDKSelfContained=true -p:PublishReadyToRun=false -p:DebugType=embedded
+
+Remove-Item $NewBinLocation -Recurse
+Copy-Item -Path "bin\Release\$NetWindowsVer\win10-x64\publish" -Destination "..\..\..\CHSongCompressorPublish\win10-x64" -Recurse
+
+Get-ChildItem -Path $NewBinLocation *.mui -Recurse | foreach { Remove-Item -Path $_.FullName }
+
+$folders = Get-ChildItem -Path $NewBinLocation -Directory -Recurse |
+Where-Object { 
+    (Get-ChildItem -Path $_.FullName -File -Recurse).Count -eq 0
+}
+$folders | Remove-Item -Force -Recurse
+
+}
+catch
+{
+    Write-Error $_.Exception.ToString()
+    Read-Host -Prompt "The above error occurred. Press Enter to exit."
+}
+
+Read-Host "Press any key to continue..."
