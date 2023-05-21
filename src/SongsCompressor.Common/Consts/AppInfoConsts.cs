e2a84ce5e063faa533400fa8bf741d92dbb58552
Krystian Leśniak <kontakt@krystianlesniak.pl>
2023-05-21 15:01:41 +0200

    - Created UpdateChecker service

---
+++ src/SongsCompressor.Common/Consts/AppInfoConsts.cs
@@ -0,0 +1,10 @@
+﻿namespace SongsCompressor.Common.Consts
+{
+    public static class AppInfoConsts
+    {
+        public const string RepositoryName = "Clone-Hero-Songs-Compressor";
+        public const string RepositoryAuthor = "KrystianLesniak";
+        public const string RepositoryUrl = $"https://github.com/{AppInfoConsts.RepositoryAuthor}/{AppInfoConsts.RepositoryName}";
+        public const string RepositoryReleasesUrl = $"{RepositoryUrl}/releases";
+    }
+}
--- src/SongsCompressor.WPF/MainWindow.xaml.cs
+++ src/SongsCompressor.WPF/MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System.Windows;
-using Microsoft.AspNetCore.Components.WebView.Wpf;
 using Microsoft.Extensions.DependencyInjection;
 using SongCompressor.MainManager;
 using SongsCompressor.Common.Interfaces.Services;
 using SongsCompressor.Common.Interfaces;
 using SongsCompressor.Services.Services;
 using MudBlazor.Services;
+using System.Threading.Tasks;
+using System.Reflection;
+using Ookii.Dialogs.Wpf;
+using System;
+using System.Diagnostics;
+using System.Security.Policy;
+using SongsCompressor.Common.Consts;
 
 namespace SongsCompressor.WPF
 {
@@ -17,6 +23,8 @@ namespace SongsCompressor.WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            _ = CheckForUpdates();
 
             var services = new ServiceCollection();
             services.AddWpfBlazorWebView();
@@ -31,6 +39,41 @@ namespace SongsCompressor.WPF
             services.AddSingleton<ICompressionManager, CompressionManager>();
 
             Resources.Add("services", services.BuildServiceProvider());
+        }
+
+        private async Task CheckForUpdates()
+        {
+            var localVersion = Assembly.GetEntryAssembly()?.GetName().Version;
+            if(localVersion == null )
+                return;
+
+            var updateChecker = new UpdateChecker(localVersion);
+
+            if (await updateChecker.CheckForNewGitHubVersion())
+            {
+                using TaskDialog dialog = new();
+
+                dialog.WindowTitle = $"Update to {updateChecker.LatestVersionString}";
+                dialog.MainInstruction = "New version available. Would you like to go to the download page now?";
+                dialog.Content = $"We have detected a newer version of the application: {updateChecker.LatestVersionString}. Your current version is {localVersion}. It's always a good idea to stay up to date with the latest versions of our application as they come with enhanced stability and performance improvements.";
+                dialog.EnableHyperlinks = true;
+                TaskDialogButton okButton = new(ButtonType.Ok);
+                TaskDialogButton cancelButton = new(ButtonType.Cancel);
+
+                dialog.Buttons.Add(okButton);
+                dialog.Buttons.Add(cancelButton);
+                TaskDialogButton button = dialog.ShowDialog(this);
+
+                if (button == okButton)
+                {
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = AppInfoConsts.RepositoryReleasesUrl,
+                        UseShellExecute = true
+                    };
+                    Process.Start(psi);
+                }
+            }
         }
     }
 }
