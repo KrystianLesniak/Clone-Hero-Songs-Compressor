8417e5656e0bc7314fdb0295c1e1aa3e8a82ef51
Krystian Leśniak <kontakt@krystianlesniak.pl>
2023-05-21 18:36:16 +0200

    - Added Message box for update into blazor

--- src/SongsCompressor.WPF/MainWindow.xaml.cs
+++ src/SongsCompressor.WPF/MainWindow.xaml.cs
@@ -24,8 +24,6 @@ namespace SongsCompressor.WPF
         {
             InitializeComponent();
 
-            _ = CheckForUpdates();
-
             var services = new ServiceCollection();
             services.AddWpfBlazorWebView();
 #if DEBUG
@@ -39,41 +37,6 @@ namespace SongsCompressor.WPF
             services.AddSingleton<ICompressionManager, CompressionManager>();
 
             Resources.Add("services", services.BuildServiceProvider());
-        }
-
-        private async Task CheckForUpdates()
-        {
-            var localVersion = Assembly.GetEntryAssembly()?.GetName().Version;
-            if(localVersion == null )
-                return;
-
-            var updateChecker = new UpdateChecker(localVersion);
-
-            if (await updateChecker.CheckForNewGitHubVersion())
-            {
-                using TaskDialog dialog = new();
-
-                dialog.WindowTitle = $"Update to {updateChecker.LatestVersionString}";
-                dialog.MainInstruction = "New version available. Would you like to go to the download page now?";
-                dialog.Content = $"We have detected a newer version of the application: {updateChecker.LatestVersionString}. Your current version is {localVersion}. It's always a good idea to stay up to date with the latest versions of our application as they come with enhanced stability and performance improvements.";
-                dialog.EnableHyperlinks = true;
-                TaskDialogButton okButton = new(ButtonType.Ok);
-                TaskDialogButton cancelButton = new(ButtonType.Cancel);
-
-                dialog.Buttons.Add(okButton);
-                dialog.Buttons.Add(cancelButton);
-                TaskDialogButton button = dialog.ShowDialog(this);
-
-                if (button == okButton)
-                {
-                    var psi = new ProcessStartInfo
-                    {
-                        FileName = AppInfoConsts.RepositoryReleasesUrl,
-                        UseShellExecute = true
-                    };
-                    Process.Start(psi);
-                }
-            }
         }
     }
 }
