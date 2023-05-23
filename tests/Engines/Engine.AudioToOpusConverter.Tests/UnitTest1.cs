fe417e0004053bac530c002d22387992bd184905
Krystian Leśniak <kontakt@krystianlesniak.pl>
2023-05-23 12:00:54 +0200

    - Added rest of Unit Tests project

---
+++ tests/Engines/Engine.AudioToOpusConverter.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Engine.AudioToOpusConverter.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
