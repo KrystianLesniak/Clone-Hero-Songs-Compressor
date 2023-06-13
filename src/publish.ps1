3ef6a938d66d0c0c73029ac9e5acaad84c470ffc
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-06-13 21:41:06 +0200

    Feature: Created workflow for publishing an application

---
+++ src/publish.ps1
@@ -0,0 +1,15 @@
+# TODO: Refactor it to use variables
+
+# Publish an application
+if (Test-Path "publish") {
+    Remove-Item "publish" -Recurse -Force
+}
+
+dotnet publish "SongsCompressor.WPF/SongsCompressor.WPF.csproj" -c Release -r win-x64 --self-contained false --property:PublishDir="../publish/CH Songs Compressor"
+
+# Compress an application into zip
+if (Test-Path "application.zip") {
+    Remove-Item "application.zip"
+}
+
+Compress-Archive -Path publish/* -DestinationPath application.zip
