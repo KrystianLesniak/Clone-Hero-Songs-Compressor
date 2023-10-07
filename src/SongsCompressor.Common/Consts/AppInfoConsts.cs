a1b4f8dc99111bce44e4d866546b08d44abf8031
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-10-07 12:10:06 +0200

    Quick-Fix: Change repository releases url to repository latest release url for update checker

--- src/SongsCompressor.Common/Consts/AppInfoConsts.cs
+++ src/SongsCompressor.Common/Consts/AppInfoConsts.cs
@@ -5,6 +5,6 @@
         public const string RepositoryName = "Clone-Hero-Songs-Compressor";
         public const string RepositoryAuthor = "KrystianLesniak";
         public const string RepositoryUrl = $"https://github.com/{AppInfoConsts.RepositoryAuthor}/{AppInfoConsts.RepositoryName}";
-        public const string RepositoryReleasesUrl = $"{RepositoryUrl}/releases";
+        public const string LatestReleasesUrl = $"{RepositoryUrl}/releases/latest";
     }
 }
